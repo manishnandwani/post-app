28623228fb6d7f0225161058dff56eb78fcba96f
prabudhb2 <prabudhb23@gmail.com>
2019-12-19 00:09:35 +0530

    homeCtrl

--- public/controllers/HomeCtrl.js
+++ public/controllers/HomeCtrl.js
@@ -1,19 +1,64 @@
 angular.module("postApp",[])
-.controller("homeCtrl",['$scope',function($scope){
 
+.controller("homeCtrl",['$scope','$http',function($scope,$http){
+    $scope.flag = 1
+    $scope.spinner = false
+    $scope.addSpinner = false
+    $scope.upVoteSpinner = false
     $scope.getPosts = function(){
-        $http.get("/list").then(function(response) {
-            if(response.data.status == "success"){
-                
-                console.log("orgdrop",response)
-            }else{
-                console.log("error");
+    $scope.spinner = true        
+        $http({
+            method: 'GET',
+            url: "/list",
+            headers: {
+                "Content-Type": "text/plain",
+                "X-Login-Ajax-call": 'true',
+                "Access-Control-Allow-Origin": null
             }
-            angular.element('#ajax-loader').hide();
-        }); 
-    }
-    
-    $scope.list =[1,2,3,4,5]
+        }).then(function(response) {
+            $scope.spinner = false
+            console.log("get",response)
+            // if(response.data.status == "success"){
+            //     $scope.list = response.data
+            //     console.log("orgdrop",response)
+            // }else{
+            //     console.log("error");
+            // }
+        }).catch(function(err){
+            $scope.spinner = false        
 
+            console.log("err",err)
+        })
+    }
+    $scope.getPosts()
+    $scope.list = [{name :"1",createdOn : new Date(),votes : 1}]
+
+    $scope.addPost = function(){
+        $scope.addSpinner = true        
+        $http.post("/add",{name: $scope.post},{
+            headers: {'Content-Type': 'text/plain'}
+		}).then(function(response){
+            $scope.addSpinner = false        
+            $scope.getPosts()
+            console.log("add",response)
+        }).catch(function(err){
+            $scope.addSpinner = false        
+            console.log(err)
+        })
+    }
+
+    $scope.upVote = function(item){
+        console.log("test item",item)
+        $scope.upVoteSpinner = true
+        $http.put('/edit',{name : item.name, votes : item.votes++}).then(function(response){
+            console.log("add",response)
+            $scope.upVoteSpinner = false
+            $scope.getPosts()
+        }).catch(function(err){
+            $scope.upVoteSpinner = false
+            console.log(err)
+        })
+    }
 
 }])
+
--- server/index.js
+++ server/index.js
@@ -1,14 +1,49 @@
 var express=require('express');
-
 var server = express()
+var bodyParser = require('body-parser');
+
+var mongodb = require("mongodb");
+var MongoClient = mongodb.MongoClient;
 
 server.use(express.static('../public/view'));
+server.use(bodyParser.json())
 
-server.get("/home",function(req,res){
-        res.json({user:"Hello world"})
+var url = "mongodb://localhost:27017";
+var db;
+
+MongoClient.connect(url,function(err,client){
+    db = client.db("demoDB");
+    console.log("error :",err);
 })
 
+server.get("/list",function(req,res){
+    console.log("test")
+    db.collection("posts").find({}).toArray(function(err,results){
+        console.log("test",results)
+        res.json(results)
+    })
+})
 
-server.listen(8081,function(){
+server.post("/add",function(req,res){
+    console.log("body",req.body)
+    db.collection("posts").insertOne(req.body,function(err,results){
+        console.log("lets0---",results,err)
+        if(err == null){
+            res.json(req.body)
+        }
+    })
+})
+
+server.put("/add",function(req,res){
+    console.log("body",req.body)
+    db.collection("posts").updateOne({name : req.params.name},{$set:{votes : req.params.votes}},function(err,results){
+        console.log("lets0---",results,err)
+        if(err == null){
+            res.json(req.body)
+        }
+    })
+})
+
+server.listen(8000,function(){
     console.log("server started")
 })
